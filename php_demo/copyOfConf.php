3eab590fa63898103cc2dff9cd2a2f43eb3914f2
unwosu6 <70655508+unwosu6@users.noreply.github.com>
2022-04-19 19:28:10 -0400

    php_demo added

---
+++ php_demo/copyOfConf.php
@@ -0,0 +1,9 @@
+
+<?php
+	//these are credentials which will be used by open.php to create connection to dbase
+	//make updates here as noted below, and save this file as conf.php
+	$dbhost = "dbase.cs.jhu.edu"; // database host
+	$dbuser = "22sp_jhopkin1";    // username on db host: replace 22sp_jhopkin1 with yours
+	$dbpass = "*****";            // password on db host: replace ***** with yours
+	$dbname = "22sp_jhopkin1_db"; // the db's name: replace 22sp_jhopkin1_db with yours
+?>
